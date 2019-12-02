36eb77f86dcf89dfd55dcc5c4be91496abf94ff9
konodiodaa <44199257+konodiodaa@users.noreply.github.com>
2019-12-02 09:32:47 +0800

    Add files via upload

---
+++ bbsforum/config.py
@@ -0,0 +1,17 @@
+
+import os
+
+DEBUG = True
+
+SECRET_KEY = os.urandom(24)
+
+DB_USERNAME = 'root'
+DB_PASSWORD = '1q1w1e2e3q3w3e'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'bbsforum'
+
+DB_URI = 'mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8' % (DB_USERNAME,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLCHEMY_TRACK_MODIFICATION = False
---
+++ bbsforum/manage.py
@@ -0,0 +1,18 @@
+
+from flask_script import Manager
+from flask_migrate import MigrateCommand, Migrate
+from app import app
+from exts import  db
+
+manage = Manager(app)
+
+
+# bonding Migrate and app, db
+migrate = Migrate(app, db)
+
+# add migrate script to manager
+manage.add_command('db', MigrateCommand)
+
+
+if __name__ == "__main__":
+    manage.run()
